c9250a39569d849bc69705150597e6226feb4ff6
Fatemeh Ghasemi <fatemeh.ghasemi.email@gmail.com>
2026-01-26 18:36:15 +0330

    refactor(mappings): remove unused mapping configurations

--- src/core/Application/Common/Mappings/MappingConfig.cs
+++ src/core/Application/Common/Mappings/MappingConfig.cs
@@ -11,10 +11,6 @@ public static class MappingConfig
 {
     public static void RegisterMappings()
     {
-        TypeAdapterConfig<CategoryItem, CategoryDto>.NewConfig();
-        TypeAdapterConfig<NoteItem, NoteDto>.NewConfig();
-        TypeAdapterConfig<GetNoteByIdResponse, NoteDto>.NewConfig();
-
         TypeAdapterConfig<Category, CategoryItem>.NewConfig()
             .Map(dest => dest.NotesCount, src => src.Notes != null ? src.Notes.Count : 0);
 
@@ -26,10 +22,6 @@ public static class MappingConfig
             .Map(dest => dest.CategoryName, src => src.Category != null ? src.Category.Title : null)
             .Map(dest => dest.Tags, src => src.NoteTags != null ? src.NoteTags.Select(nt => nt.Tag.Title).ToList() : new List<string>());
 
-        TypeAdapterConfig<Note, NoteResponse>.NewConfig();
-
-        TypeAdapterConfig<Category, CategoryResponse>.NewConfig();
-        
         TypeAdapterConfig<Category, GetCategoryByIdResponse>.NewConfig()
             .Map(dest => dest.NotesCount, src => src.Notes != null ? src.Notes.Count : 0)
             .Map(dest => dest.Notes, src => src.Notes != null ? src.Notes.Select(n => new CategoryNoteItem
@@ -40,41 +32,11 @@ public static class MappingConfig
                 CreatedAt = n.CreatedAt
             }).ToList() : new List<CategoryNoteItem>());
 
-        TypeAdapterConfig<GetCategoryByIdResponse, CategoryDto>.NewConfig();
-
-        TypeAdapterConfig<Tag, TagResponse>.NewConfig();
-
-        TypeAdapterConfig<Tag, TagItem>.NewConfig();
-
         TypeAdapterConfig<Tag, GetTagByIdResponse>.NewConfig()
             .Map(dest => dest.NotesCount, src => src.NoteTags != null ? src.NoteTags.Count : 0);
 
-        TypeAdapterConfig<CreateNoteDto, Note>.NewConfig()
-            .Map(dest => dest.Id, src => Guid.NewGuid())
-            .Map(dest => dest.CreatedAt, src => DateTime.UtcNow)
-            .Ignore(dest => dest.UpdatedAt)
-            .Ignore(dest => dest.Category)
-            .Ignore(dest => dest.NoteTags)
-            .Ignore(dest => dest.Color);
-
-        TypeAdapterConfig<UpdateNoteDto, Note>.NewConfig()
-            .Map(dest => dest.UpdatedAt, src => DateTime.UtcNow)
-            .Ignore(dest => dest.Id)
-            .Ignore(dest => dest.CreatedAt)
-            .Ignore(dest => dest.Category)
-            .Ignore(dest => dest.NoteTags)
-            .Ignore(dest => dest.Color);
-
-        TypeAdapterConfig<CreateCategoryDto, Category>.NewConfig()
-            .Map(dest => dest.Id, src => Guid.NewGuid())
-            .Map(dest => dest.CreatedAt, src => DateTime.UtcNow)
-            .Ignore(dest => dest.UpdatedAt)
-            .Ignore(dest => dest.Notes);
-
-        TypeAdapterConfig<UpdateCategoryDto, Category>.NewConfig()
-            .Map(dest => dest.UpdatedAt, src => DateTime.UtcNow)
-            .Ignore(dest => dest.Id)
-            .Ignore(dest => dest.CreatedAt)
-            .Ignore(dest => dest.Notes);
+        TypeAdapterConfig<GetNoteByIdResponse, NoteDto>.NewConfig();
+        TypeAdapterConfig<CategoryItem, CategoryDto>.NewConfig();
+        TypeAdapterConfig<NoteItem, NoteDto>.NewConfig();
     }
 }
